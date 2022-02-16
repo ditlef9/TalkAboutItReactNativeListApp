e5466b052aade9200ffa34b1b4ddedc446886e6a
Sindre <sindredit@gmail.com>
2022-02-16 20:18:34 +0100

    m

--- components/KeywordInput.js
+++ components/KeywordInput.js
@@ -3,7 +3,6 @@ import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 /* 2:30 */
 
-
 const KeywordInput = props => {
     const [enteredKeyword, setEnteredKeyword] = useState(''); /* Bind TextInput to the variables */
 
