b2bd42931141ffb17aaa9aeff6eca1579973a283
Sindre <sindredit@gmail.com>
2022-02-16 20:15:01 +0100

    Master

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
+
+import KeywordInput from './components/KeywordInput';
+import KeywordItem from './components/KeywordItem';
 
 export default function App() {
+
+  const [keywordsArray, setKeywordsArray] = useState([]);  /* Array of keywords */
+
+  
+  const addKeywordHandler = keywordTitle => {
+    setKeywordsArray(currentKeywords => [
+      ...currentKeywords, 
+      { id: Math.random().toString(), value: keywordTitle }
+    ]); /* Takes old arrays and puts into new array */
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      { /* Header */ }
+      <KeywordInput onAddKeyword={addKeywordHandler} />
+
+      { /* Output array of keywords */ }
+      <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={keywordsArray} renderItem={itemData => <KeywordItem title={itemData.item.value} /> }
+      />
     </View>
   );
 }
@@ -13,8 +35,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f2f2f2',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+
+
+
+  main: {
+    textAlign: 'right'
+  },
+  arrayItem: {
+    padding: 10,
+    marginBottom: 2,
+    width: "100%", 
+    backgroundColor: '#ffffff',
+    borderColor: '#dadde1',
+    borderWidth: 1
+  }
 });
